a9672d04a8a3472250c8f6fd0ccd612ee9cd91d2
Sirawat <trtor12@gmail.com>
2021-06-27 20:09:04 +0700

    test mock database for CI

---
+++ test/__mock__/3-insert-TStudy.sql
@@ -0,0 +1,49 @@
+USE TStudyTabCI;
+
+INSERT INTO [TStudyTab]([StudyKey], [std_seq], [StudyInsUID], [PID], [PNAME], [EngName], [StudyID], [StudyDesc], [Modality], [Bodypart], [Room], [StudyDate], [StudyTime], [AccessNum], [PhysicianName], [PatAge], [SeriesCnt], [ImageCnt], [InsertDate], [InsertTime], [ArchStatus], [ExamStatus], [CompStatus], [Comments], [DelFlag], [SendStatus], [SockStatus], [VerifyFlag], [VerifyTime], [AccessTime], [AccessCnt], [FetchReqTime], [Dept], [ReportDate], [ReportTime], [ReportLoc], [Report], [ReportDoctor], [VerifyDoctor], [PStatus], [ReportDT], [ReceiveFlag], [ReceiveDate], [ReceiveTime], [ReceiveBy], [Amount], [Clinic], [ChargeType], [Charge], [NM]) VALUES (25940983, NULL, NULL, N'52569347', 'ประจญ กิ่งมิ่งแฮ                                                ', 'PRAJON KINGMINGHAE', NULL, NULL, N'CR', 'CHEST PA&LATERAL', 'CR0       ', '2020-02-04 16:43:28.000', '2020-02-04 16:43:57.000', NULL, NULL, N'71', NULL, NULL, '2020-02-04 16:10:26.193', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2020-02-04 17:38:00.710', NULL, NULL, NULL, NULL, N'04/02/2020', N'17:37', '                         ', N'FILM CXR PA UPRIGHT AND LATERAL VIEW
+
+FINDINGS : 
+
+- No active pulmonary infiltration is noted.
+
+- Clear retrostenal space.
+
+- Unremarkable both hili. 
+
+- No pneumothorax or pleural effusion is detected.
+
+- Normal heart size is observed.
+
+- Old non-union fracture distal right clavicle.
+
+
+
+
+
+', '                                ', 'Pattarawit Withayasuk           ', 'VERIFY    ', '2020-02-04 17:37:58.603', NULL, NULL, NULL, NULL, NULL, 'CLINIC', '  ', 500, NULL); 
+
+INSERT INTO [TStudyTab]([StudyKey], [std_seq], [StudyInsUID], [PID], [PNAME], [EngName], [StudyID], [StudyDesc], [Modality], [Bodypart], [Room], [StudyDate], [StudyTime], [AccessNum], [PhysicianName], [PatAge], [SeriesCnt], [ImageCnt], [InsertDate], [InsertTime], [ArchStatus], [ExamStatus], [CompStatus], [Comments], [DelFlag], [SendStatus], [SockStatus], [VerifyFlag], [VerifyTime], [AccessTime], [AccessCnt], [FetchReqTime], [Dept], [ReportDate], [ReportTime], [ReportLoc], [Report], [ReportDoctor], [VerifyDoctor], [PStatus], [ReportDT], [ReceiveFlag], [ReceiveDate], [ReceiveTime], [ReceiveBy], [Amount], [Clinic], [ChargeType], [Charge], [NM]) VALUES (25935548, NULL, NULL, N'52569347', 'ประจญ กิ่งมิ่งแฮ                                                ', 'PRAJON KINGMINGHAE', NULL, NULL, N'CR', 'CHEST PA&LATERAL', 'CR5       ', '2020-01-31 14:19:57.000', '2020-01-31 14:20:00.000', NULL, NULL, N'71', NULL, NULL, '2020-01-31 14:10:51.693', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2020-01-31 14:35:22.627', NULL, NULL, NULL, NULL, N'31/01/2020', N'14:33', '                         ', N'
+
+
+
+FILM CXR PA UPRIGHT AND LATERAL VIEW
+
+FINDINGS : 
+
+- No active pulmonary infiltration is noted.
+
+- Clear retrostenal space.
+
+- Unremarkable both hili. 
+
+- No pneumothorax or pleural effusion is detected.
+
+- Normal heart size is observed.
+
+- Bony structures show old fracture distal right clavicle.
+
+
+
+
+
+', '                                ', 'Doonyaporn Wongsawaeng          ', 'VERIFY    ', '2020-01-31 14:33:04.923', NULL, NULL, NULL, NULL, NULL, 'ในเวลาราชการ', '  ', 500, NULL); 
--- test/__mock__/ci-run-sql.ts
+++ test/__mock__/ci-run-sql.ts
@@ -9,6 +9,24 @@ const createDB = fs.readFileSync(
   "utf8",
 );
 
+const createTable1 = fs.readFileSync(
+  path.join(__dirname, "./2-create-table-TStudy.sql"),
+  "utf8",
+);
+const createTable2 = fs.readFileSync(
+  path.join(__dirname, "./2-create-table-Appoint.sql"),
+  "utf8",
+);
+
+const insertPacplusmaster =
+  fs.readFileSync(path.join(__dirname, "./3-insert-risevent.sql"), "utf8") +
+  fs.readFileSync(path.join(__dirname, "./3-insert-TStudy.sql"), "utf8");
+
+const insertAppoint = fs.readFileSync(
+  path.join(__dirname, "./3-insert-appoint.sql"),
+  "utf8",
+);
+
 // async/await style:
 const pool1 = new sql.ConnectionPool({
   server: process.env.DB_RAD_37_IP ?? "",
@@ -25,6 +43,9 @@ const pool1_conn = pool1.connect();
     await pool1_conn;
     const request = pool1.request();
     await request.query(createDB);
+    await request.query(createTable1 + createTable2);
+    await request.query(insertPacplusmaster);
+    await request.query(insertAppoint);
 
     // Close connection
     await pool1.close();
